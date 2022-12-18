48d1f0e20e4944cedf4e9d9c68f8b88e25ef12c0
saeed <es-amgaballah24@alexu.edu.eg>
2022-12-18 20:33:41 +0200

    feat: create command parser

--- client.c
+++ client.c
@@ -7,24 +7,57 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 #define PORT 8080 // the port client will be connecting to
 
 #define MAXDATASIZE 1024 // max number of bytes we can get at once
 
-int main(int argc, char const *argv[])
-{
-    int sock_fd = 0, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[MAXDATASIZE] = {0};
+int sock_fd = 0, valread, client_fd;
+struct sockaddr_in serv_addr;
+char *hello = "Hello from client";
+char buffer[MAXDATASIZE] = {0};
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: client hostname\n");
-        exit(1);
+struct Command
+{
+    char *type;
+    char *filePath;
+    char *hostName;
+    char *port;
+} command;
+
+void parse_single_command()
+{
+    char *token = strtok(buffer, " ");
+    command.type = token;
+
+    token = strtok(NULL, " ");
+    command.filePath = token;
+
+    token = strtok(NULL, " ");
+    command.hostName = token;
+
+    token = strtok(NULL, " ");
+    command.filePath = token;
+}
+
+void parse_commands()
+{
+    FILE *fptr = fopen("client_commands.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("no such file.");
+        return;
     }
 
+    while (fgets(buffer, MAXDATASIZE, fptr) != NULL)
+    {
+        parse_single_command();
+
+    }
+}
+
+int main(int argc, char const *argv[])
+{
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
