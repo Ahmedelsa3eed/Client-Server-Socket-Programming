b26e19dc4bab47da8ede4f4321ada7d231855938
elemary <honor5766@gmail.com>
2022-12-22 03:33:03 +0200

    solve server timeOut

--- server.c
+++ server.c
@@ -14,6 +14,7 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <sys/time.h>
+#include <libgen.h>
 
 #define PORT 8080 // the port users will be connecting to
 
@@ -28,8 +29,8 @@ typedef enum RequestType {
 } RequestType;
 
 typedef struct timeOutCalc {
-    clock_t *start;
-    int *loc;
+    clock_t start;
+    int loc;
     int maxTime;
     int new_fd;
 } timeOutCalc;
@@ -72,24 +73,20 @@ char *readFile(char *fileName)
 {
     FILE *fp;
     char *content = (char *) malloc(10000);
-    fp = fopen(fileName, "r");
+    fp = fopen(fileName, "rb");
     if (fp == NULL)
         return NULL;
-    char c = fgetc(fp);
-    int i = 0;
-    while (c != EOF) {
-        content[i] = c;
-        c = fgetc(fp);
-        i++;
+    unsigned int nBytes = 0;
+    while(fread(&content[nBytes], sizeof(char), 1, fp) == 1) {
+        nBytes++;
     }
-    content[i] = '\0';
     fclose(fp);
     return content;
 }
 
 void sendResponse(int client_fd, char *response)
 {
-    send(client_fd, response, strlen(response), 0);
+    send(client_fd, response, strlen(response), MSG_NOSIGNAL);
 }
 
 void handleGetRequest(int client_fd, char *request)
@@ -103,6 +100,26 @@ void handleGetRequest(int client_fd, char *request)
         sendResponse(client_fd, content);
     }
 }
+void saveFile(char *filePath, size_t nBytes, char *buffer)
+{
+    char *newfilePath = (char *) malloc(50);
+    strcat(newfilePath, "serverPostFiles/");
+    strcat(newfilePath, basename(filePath));
+
+    FILE *fptr;
+    if ((fptr = fopen(newfilePath, "wb")) == NULL) {
+        fprintf(stderr, "Couldn't open file for writing. \n");
+        return;
+    }
+    int toBeWritten = 0;
+    while (toBeWritten < nBytes) {
+        fwrite(&buffer[toBeWritten], sizeof(char), 1, fptr);
+        toBeWritten++;
+    }
+    printf("Received file saved in: %s\n", newfilePath);
+    free(newfilePath);
+    fclose(fptr);
+}
 
 void handlePostRequest(int client_fd, char *request)
 {
@@ -110,33 +127,34 @@ void handlePostRequest(int client_fd, char *request)
     int valread;
     char buffer[1024] = {0};
     valread = read(client_fd, buffer, 1024);
-    printf("server: message is %s\n", buffer);
+    saveFile( "ay haga", valread, buffer);
 }
 
 void *calculateTimeOut(void *arg)
 {
     timeOutCalc *timeOut = (timeOutCalc *) arg;
-    clock_t *start = timeOut->start;
-    int *loc = timeOut->loc;
-    int maxTime = timeOut->maxTime;
-    int new_fd = timeOut->new_fd;
+    printf( "I am in the time out thread");
 
-    while ( (double)(clock() - *start) < maxTime * CLOCKS_PER_SEC ) {// wait for 10 seconds to get the request from the client
+    while ( (double)( (clock_t) clock() - timeOut->start)/ CLOCKS_PER_SEC < timeOut->maxTime ) {// wait for 10 seconds to get the request from the client
 
-        if (*loc == 1) { // if the request is received
-            printf("server receive request from client %d\n", new_fd);
+        if (timeOut->loc == 1) { // if the request is received
+            printf("server receive request from client %d\n",  timeOut->new_fd);
             printf("server received request at %ld\n", clock());
-            
-            while (*loc == 1) {
+
+            while (timeOut->loc == 1) {
                 // wait for the client to send the request
             }
             printf("Waited for 10 seconds, closing the connection\n");
-            printf("new start time is %ld\n", *start);
+            printf("new start time is %ld\n", timeOut->start);
             // start get updated in the client thread and the same for the loc
         }
+//        printf(" clock is %ld\n", (clock_t) clock()/CLOCKS_PER_SEC);
+//        printf("start is %ld\n", timeOut->start/CLOCKS_PER_SEC);
+//        printf("I am in the time out thread time is %f\n", (double)( clock() - timeOut->start ) / CLOCKS_PER_SEC);
     }
-    *start = -1;// reset the start time to -1 to indicate that the connection is closed
-    close(new_fd); // close the connection
+    printf("Waited for %d seconds, closing the connection\n", timeOut->maxTime);
+    timeOut->start = -1;// reset the start time to -1 to indicate that the connection is closed
+    close( timeOut->new_fd); // close the connection
     pthread_exit(NULL);// exit the thread
 }
 
@@ -148,22 +166,33 @@ this function will be called to handle each client
 void *handleClient(void *new_fd) {
     // get the argument from the thread
     int client_fd = *(int *) new_fd;
-    clock_t startTimeInMillis = clock();
-    int loc = 0;
-    timeOutCalc *timeOut = {startTimeInMillis, loc, 10, client_fd};
+   // clock_t startTimeInMillis = clock();
+
+    timeOutCalc *timeOut = (timeOutCalc *) malloc(sizeof(timeOutCalc)) ;
+    timeOut->start = (clock_t) clock();
+    timeOut->loc = 0;
+    timeOut->maxTime = 10;
+    timeOut->new_fd = client_fd;
+
     pthread_t timeOutThread;
-    pthread_create(&timeOutThread, NULL, calculateTimeOut, &timeOut);
+    pthread_create(&timeOutThread, NULL, calculateTimeOut, timeOut);
     int valread;
     char buffer[1024] = {0};
 
-    while (startTimeInMillis != -1){
-        loc = 0 // Make the calculateTimeOut thread start waiting the time out if the client doesn't send the request in 10 seconds
+    while (timeOut->start != -1){
+        timeOut->loc  = 0; // Make the calculateTimeOut thread start waiting the time out if the client doesn't send the request in 10 seconds
+        memset(buffer, 0, sizeof buffer);
+        // wait reading from the client
         valread = read(client_fd, buffer, 1024);
-        loc = 1;// Make the calculateTimeOut thread stop waiting the time out if the client sends the request in 10 seconds
+        if (valread == 0) {
+            printf("client %d closed the connection\n", client_fd);
+            break;
+        } else if (valread == -1) {
+            printf("error reading from the client %d\n", client_fd);
+            break;
+        }
+        timeOut->loc  = 1;// Make the calculateTimeOut thread stop waiting the time out if the client sends the request in 10 seconds
         RequestType requestType = parseRequest(buffer);
-        printf("server: message is %s\n", buffer);
-        printf("request type: %d\n", parseRequest(buffer));
-
         if (requestType == GET) {
             printf("GET request\n");
             handleGetRequest(client_fd, buffer);
@@ -174,17 +203,18 @@ void *handleClient(void *new_fd) {
             printf("Invalid request\n");
             sendResponse(client_fd, not_found);
         }
-        startTimeInMillis = clock();// reset the start time for the calculateTimeOut thread then it will start waiting for 10 seconds again
+        // reset the start time for the calculateTimeOut thread then it will start waiting for 10 seconds again
+        timeOut->start = (clock_t) clock();
     }
 
     // here I should wait time out till reciving an other request or close the connection
-    close(client_fd);
     return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
     // listen on server_fd, new connection on new_fd
+    signal(SIGPIPE, SIG_IGN);
     int server_fd, new_fd;
     struct sockaddr_in address;
     int opt = 1;
